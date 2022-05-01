e667b75d0256a03f9a7cb73fea184453dbd0d586
Muhammed Ali Demir <bymadmr@hotmail.com>
2022-05-01 14:14:55 +0300

    17. gün ortası

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'WebProject';
+  title:string = 'WebProject';
+  user:string = "Ali Demir";
+
 }
+
