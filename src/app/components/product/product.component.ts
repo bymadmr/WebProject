22c999c96270ae94c5c925739be4184785c12903
Muhammed Ali Demir <bymadmr@hotmail.com>
2022-05-02 23:57:36 +0300

    toastr notification added

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-product',
@@ -14,7 +15,7 @@ export class ProductComponent implements OnInit {
   filterText="";
   constructor(
     private productService: ProductService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,private toastrService: ToastrService
   ) {}
 
   ngOnInit(): void {
@@ -38,5 +39,9 @@ export class ProductComponent implements OnInit {
       this.products = response.data;
       this.dataLoaded = true;
     });
+  }
+  addToCart(product:Product){
+    this.toastrService.success('Sepete eklendi', product.productName);
+    console.log(product.productName)
   }
 }
