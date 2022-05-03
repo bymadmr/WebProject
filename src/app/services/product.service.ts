9f1098bd641390598342113d38c34016a67f26e3
Muhammed Ali Demir <bymadmr@hotmail.com>
2022-05-04 02:12:51 +0300

    response model update

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +19,9 @@ export class ProductService {
   getProductsByCategory(categoryId:number): Observable<ListResponseModel<Product>> {
     let newPath=this.apiUrl+"Products/getbycategory?categoryId="+categoryId
     return this.httpClient.get<ListResponseModel<Product>>(newPath);
+  }
+  add(product:Product):Observable<ResponseModel>{
+    let newPath=this.apiUrl+"Products/add"
+    return this.httpClient.post<ResponseModel>(newPath,product);
   }
 }
